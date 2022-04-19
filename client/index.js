f2f46de3199a96def2a1d1215aa3b0c8cb531001
Simon <simonlee1125@gmail.com>
2022-04-19 13:11:27 -0400

    4/19/22 1:11pm

---
+++ client/index.js
@@ -0,0 +1,24 @@
+/**
+ * ************************************
+ *
+ * @module  index.js
+ * @author Simon Lee
+ * @date 4/19/2022
+ * @description entry point for application. Hangs React app off of #contents in index.html
+ *
+ * ************************************
+ */
+
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './App.jsx';
+import store from './store';
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+  // wrap the App in the Provider Component and pass in the store
+  document.getElementById('screen')
+);
---
+++ client/scripts.js
@@ -0,0 +1,5 @@
+const map = new google.maps.Map(document.getElementById('map'), {
+  center: { lat: -34.397, lng: 150.644 },
+  zoom: 8,
+  mapId: '96d39a677dc4e115',
+});
